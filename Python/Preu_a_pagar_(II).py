1aa1e7141b97524431382cdc52fd754b31e30755
Jowy32 <44445315+Jowy32@users.noreply.github.com>
2018-10-29 16:10:36 +0100

    Update Preu_a_pagar_(II).py

--- Python/Preu_a_pagar_(II).py
+++ Python/Preu_a_pagar_(II).py
@@ -1,6 +1,6 @@
 #Python 3.7
 Edat=int(input("Quina edat tens?:"))
 if (Edat < 5) or (Edat >= 65):
-  print ("No pagues")
+    print ("No pagues")
 else:
-  print ("Pagues 2,15€")
+    print ("Pagues 2,15€")
