bdf68a6c7e8fe64a8d18ff9f4a01667ff1812220
Jowy32 <44445315+Jowy32@users.noreply.github.com>
2018-10-29 16:12:12 +0100

    Update ejercicio-edad-3.py

--- Python/ejercicio-edad-3.py
+++ Python/ejercicio-edad-3.py
@@ -1,4 +1,5 @@
-edad=int(input("Que edat tienes?:"))
+#Python 3.6
+edad=int(input("Que edad tienes?:"))
 if (edad>=18)and(edad<=23):
     print ("puedes entrar a la sesion de jovenes")
 if (edad==17):
