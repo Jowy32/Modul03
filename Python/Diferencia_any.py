7d7a6a4f0f8ac2ea2aa04bf21178750c9f99d658
Jowy32 <44445315+Jowy32@users.noreply.github.com>
2018-11-07 18:33:17 +0100

    Update Diferencia_any.py

--- Python/Diferencia_any.py
+++ Python/Diferencia_any.py
@@ -1,11 +1,14 @@
 Any_actual = int(input("Quin any es?:"))
 Any_qualsevol = int(input("Escriu un any qualsevol:"))
-if (Any_actual == Any_qualsevol):
-    print ("Son el mateix any!")
+if (Any_actual == 0)or(Any_qualsevol == 0)
+    print ("L'any 0 no existeix")
 else:
-    if (Any_actual < Any_qualsevol):
-        res = (Any_qualsevol) - (Any_actual)
-        print ("Per arribar al any",Any_qualsevol,"falten",res," anys")
+    if (Any_actual == Any_qualsevol):
+        print ("Son el mateix any!")
     else:
-        res = (Any_actual) - (Any_qualsevol)
-        print ("Han passat",res,"anys desde l'any",Any_qualsevol,)
+        if (Any_actual < Any_qualsevol):
+            res = (Any_qualsevol) - (Any_actual)
+            print ("Per arribar al any",Any_qualsevol,"falten",res," anys")
+        else:
+            res = (Any_actual) - (Any_qualsevol)
+            print ("Han passat",res,"anys desde l'any",Any_qualsevol,)
