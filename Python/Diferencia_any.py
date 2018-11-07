594edf816eb876836cab9872dbe19a39eee7ad4e
Jowy32 <44445315+Jowy32@users.noreply.github.com>
2018-11-07 23:19:26 +0100

    Update Diferencia_any.py

--- Python/Diferencia_any.py
+++ Python/Diferencia_any.py
@@ -6,9 +6,16 @@ else:
     if (Any_actual == Any_qualsevol):
         print ("Son el mateix any!")
     else:
-        if (Any_actual < Any_qualsevol):
-            res = (Any_qualsevol) - (Any_actual)
-            print ("Per arribar al any",Any_qualsevol,"falten",res," anys")
-        else:
-            res = (Any_actual) - (Any_qualsevol)
+        if (Any_actual < 0)and(Any_qualsevol < 0)and(Any_actual < Any_qualsevol):
+            res = (Any_qualsevol)-(Any_actual)
+            print ("Per arribar al any",Any_qualsevol,"falten",res, "anys")
+        if (Any_actual < 0)and(Any_qualsevol < 0)and(Any_actual > Any_qualsevol):
+            res = (Any_actual)-(Any_qualsevol)
             print ("Han passat",res,"anys desde l'any",Any_qualsevol,)
+        else:
+            if (Any_actual > 0)and(Any_qualsevol > 0)and(Any_actual < Any_qualsevol):
+                res = (Any_qualsevol) - (Any_actual)
+                print ("Per arribar al any",Any_qualsevol,"falten",res," anys")
+            if (Any_actual > 0)and(Any_qualsevol > 0)and(Any_actual > Any_qualsevol):
+                res = (Any_actual) - (Any_qualsevol)
+                print ("Han passat",res,"anys desde l'any",Any_qualsevol,)
