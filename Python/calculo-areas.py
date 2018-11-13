3ece441dab337d1bdb45e671b8ad7020f7dfc6b6
Jowy32 <44445315+Jowy32@users.noreply.github.com>
2018-11-13 13:32:15 +0100

    Update calculo-areas.py

--- Python/calculo-areas.py
+++ Python/calculo-areas.py
@@ -1,12 +1,13 @@
-figura=input("Que figura quiere calcular (Escriba T o C):?")
+figura=input("Que figura quiere calcular (Escriba T o C)?:")
 if (figura == "T")or(figura == "t"):
-    base=int(input("Indique la base del triangulo:"))
-    altura=int(input("Indique la altura del triangulo:"))
+    base=float(input("Indique la base del triangulo:"))
+    altura=float(input("Indique la altura del triangulo:"))
     area=(base*altura/2)
     print ("Un triangulo de base",base,"y altura",altura,"tiene un área de",area)
-if (figura == "C")or(figura == "c"):
-    radio=int(input("Indique el radio"))
-    area=(3.1415*(radio*radio))
-    print ("Un circulo de radio",radio,"tiene un área de",area)
 else:
-    print ("No calculamos el area de la figura geometrica que desea, disculpe las molestias")
+    if (figura == "C")or(figura == "c"):
+        radio=float(input("Indique el radio"))
+        area=(3.1415*(radio*radio))
+        print ("Un circulo de radio",radio,"tiene un área de",area)
+    else:
+        print ("No calculamos el area de la figura geometrica que desea, disculpe las molestias")
