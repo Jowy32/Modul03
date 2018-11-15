1c3fdd9082598c1e41ed241fb44d79292016779d
Jowy32 <44445315+Jowy32@users.noreply.github.com>
2018-11-15 10:13:43 +0100

    Create Maximos.py

---
+++ Python/Maximos.py
@@ -0,0 +1,12 @@
+numero1 = int(input("Escriba un numero:"))
+numero2 = int(input("Escriba otro numero:"))
+if (numero1 > numero2):
+    print (numero1)
+else:
+    if (numero2 > numero1):
+        print (numero2)
+    else:
+        print ("Son el mismo numero")
+##
+##
+##
