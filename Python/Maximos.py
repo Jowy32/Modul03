f29f14ed88416f6dfb25d91a15612c88d067624e
Jowy32 <44445315+Jowy32@users.noreply.github.com>
2018-11-15 10:27:09 +0100

    Update Maximos.py

--- Python/Maximos.py
+++ Python/Maximos.py
@@ -7,3 +7,13 @@ else:
 ##
 ##
 ##
+numero1 = int(input("Escriba un numero:"))
+numero2 = int(input("Escriba otro numero:"))
+numero3 = int(input("Escriba otro numero:"))
+if (numero1 >= numero2 >= numero3):
+    print (numero1)
+else:
+    if (numero2 >= numero1 >= numero3):
+        print (numero2)
+    else:
+        print (numero3)
