9e188e33fb863c0e603154453bd8b45aae7f192e
Jowy32 <44445315+Jowy32@users.noreply.github.com>
2018-12-04 13:09:07 +0100

    Create cara_o_cruz.py

---
+++ Python/cara_o_cruz.py
@@ -0,0 +1,16 @@
+from random import randint
+lado_1 = randint (1,2)
+lado_2 = input("Elige cara o cruz:")
+if (lado_2 == "cara"):
+    lado_2 = 2
+    if (lado_2 == lado_1):
+        print ("Ha salido cara, has ganado")
+    else:
+        print ("Ha salido cruz, no has ganado")
+else:
+    if (lado_2 == "cruz"):
+        lado_2 = 2
+        if (lado_2 == lado_1):
+            print ("Ha salido cruz, has ganado")
+        else:
+            print ("Ha salido cara, no has ganado")
