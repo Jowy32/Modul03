e1cbe7a0853501139547f178517ce6c23b8fe736
Jowy32 <44445315+Jowy32@users.noreply.github.com>
2018-12-11 13:27:24 +0100

    Create practica.py

---
+++ Python/practica.py
@@ -0,0 +1,37 @@
+if (carta_2 == "A"):
+    carta_2 = 14
+else:
+    if (carta_2 == "2")
+        carta_2 = 2
+    else:
+        if (carta_2 == "3"):
+            carta_2 = 3
+        else:
+            if (carta_2 == "4"):
+                carta_2 = 4
+            else:
+                if (carta_2 == "5"):
+                    carta_2 = 5
+                else:
+                    if (carta_2 == "6")
+                        carta_2 = 6
+                    else:
+                        if (carta_2 == "7"):
+                            carta_2 = 7
+                        else:
+                            if (carta_2 == "8"):
+                                carta_2 = 8
+                            else:
+                                if (carta_2 == "9")
+                                    carta_2 = 9
+                                else:
+                                    if (carta_2 == "10"):
+                                        carta_2 = 10
+                                    else:
+                                        if (carta_2 == "J"):
+                                            carta_2 = 11
+                                            if (carta_2 == "Q"):
+                                                carta_2 = 12
+                                            else:
+                                                if (carta_2 == "K")
+                                                    carta_2 = 13
